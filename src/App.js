2bea78392f879989e08da77e960f2629003adb70
Meryem Yanar <meryem.yanaryanar@gmail.com>
2025-11-17 11:31:41 +0000

    Add dummy table component; preliminary styling of App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "@mui/material";
+import "./App.css";
+import { CurrencyTable } from "./components/Table/index.tsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Currency App</h1>
       </header>
+      <Container maxWidth="lg" className="App-container">
+        <CurrencyTable />
+      </Container>
     </div>
   );
 }
